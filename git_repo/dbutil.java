4c2eb14c911d50d121d6535d41ad124a25c963cb
Rakesh Sathua <liku28021@gmail.com>
2026-04-30 20:42:40 +0530

    Update dbutil.java

--- git_repo/dbutil.java
+++ git_repo/dbutil.java
@@ -1,6 +1,17 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        
+        String name = "Rakesh";
+        String city = "Bhubaneswar";
+        String skill = "Learning Java and Shell Scripting";
 
-Hello Guys,
-this file contain the db scripts
-
-hello sir this is the new line
-here is the new one
+        // Loop to print details 5 times
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("---- About Me (" + i + ") ----");
+            System.out.println("My name is: " + name);
+            System.out.println("I live in: " + city);
+            System.out.println("I am: " + skill);
+            System.out.println();
+        }
+    }
+}
